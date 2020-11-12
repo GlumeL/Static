3ab3250ac57c8c9cfffa4277e0af35794062568e
Glume <1034630181@qq.com>
2020-11-12 15:16:18 +0800

    Static

---
+++ Butterfly/Glume/js/domain.js
@@ -0,0 +1,31 @@
+    /*感谢云猫Cloud的api接口*/
+    function baidu_check(){
+        $.getJSON("https://blog.2w2.top/api/baidu.php?domain="+window.location.href,function(result){ 
+            if (result.code == 200) {
+                $('#baidu_icon').removeClass('fa fa-spinner glume-gd-stop');
+                $('#baidu_icon').addClass('fas fa-check-circle glume-gd-stop');
+                $('#baidu_result').text('百度已收录');
+            }else if(result.code == 403){
+                $('#baidu_icon').removeClass('fa fa-spinner glume-stop');
+                $('#baidu_icon').addClass('fas fa-times-circle glume-gd-stop');
+                $('#baidu_result').text('百度未收录');
+                baidu_push();
+            }else{
+                 $('#baidu_icon').removeClass('fa fa-spinner glume-gd-stop');
+                $('#baidu_icon').addClass('fas fa-exclamation-circle glume-gd-stop');
+                $('#baidu_result').text('查询收录失败');
+            }
+        });
+    }
+    function baidu_push(){
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+        } else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+    }
+    baidu_check();
